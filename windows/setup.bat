c94f21ed5c86b7c6a2d34cecce3799c57a170dee
Mark Dasco <mrkrynmdsco@users.noreply.github.com>
2021-12-19 13:25:20 +0800

    Update setup.bat

--- windows/setup.bat
+++ windows/setup.bat
@@ -10,16 +10,16 @@ call %CMDIR%\create-workspace.bat
 :: setup basic development tools
 
 :: archiving tool
-@REM call %CMDIR%\install-7zip.bat %UTDIR%
+call %CMDIR%\install-7zip.bat %UTDIR%
 
 :: git
-@REM call %CMDIR%\install-git.bat %ZIP%\7z.exe
+call %CMDIR%\install-git.bat %ZIP%\7z.exe
 
 :: vscode-portable
-@REM call %CMDIR%\install-vscode.bat %ZIP%\7z.exe
+call %CMDIR%\install-vscode.bat %ZIP%\7z.exe
 
 :: msys2-mingw64
-@REM call %CMDIR%\install-msys.bat %ZIP%\7z.exe %CMDIR%\bash
+call %CMDIR%\install-msys.bat %ZIP%\7z.exe %CMDIR%\bash
 
 :: systemc
 call %CMDIR%\install-systemc.bat %CMDIR%\bash
